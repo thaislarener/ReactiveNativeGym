4aec90e369957188d0b95bf21116e2f20993dc41
Calel <thaisla_renner@hotmail.com>
2023-03-20 13:25:23 -0400

    conexão com backend e alertas de erros nos inputs

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,14 +1,17 @@
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from 'native-base';
 import { useNavigation } from '@react-navigation/native';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+
+import { api } from '@services/api';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { AppError } from '@utils/AppError';
 
 type FormDataProps = {
     name: string;
@@ -25,6 +28,8 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp(){
+    const toast = useToast();
+
     const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
         resolver: yupResolver(signUpSchema)
     });
@@ -35,8 +40,20 @@ export function SignUp(){
         navigation.goBack();
     }
 
-    function handleSignUp(data: FormDataProps){
-        console.log(data);
+    async function handleSignUp({name, email, password}: FormDataProps){
+        try{
+            const response = await api.post('/users', { name, email, password });
+        }
+        catch(error){
+            const isAppError = error instanceof AppError;
+            const title = isAppError ? error.message : 'Não foi possível criar conta. Tente novamente mais tarde.'
+            toast.show({
+                title,
+                placement: 'top',
+                bgColor: 'red.500'
+            });
+        }
+        
     }
 
     return(
